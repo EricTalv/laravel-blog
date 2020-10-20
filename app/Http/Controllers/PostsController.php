c1956e1d5143a2491c598174f9b6fe9ef7c8639b
EricTalv <eric.talviste@gmail.com>
2020-10-20 17:19:01 +0300

    finished setting up DB conenctions

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PostsController extends Controller
+{
+    public function show($slug)
+    {
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+
+
+
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List of Posts</title>
+</head>
+<body>
+    <h1>Here's a List of Posts</h1>
+</body>
+</html>
