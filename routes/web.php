4c8b2cd20d28e21ee88a4be7df41dbfacfee6109
EricTalv <eric.talviste@tptlive.ee>
2020-10-23 18:37:26 +0300

    working on implementing templates to view

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('/posts', function () {
 
 Route::get('/posts/{post}', 'PostsController@show');
 
-Route::get('/contact', function () {
-    return view('contact');
+Route::get('/about', function () {
+    return view('about');
 });
