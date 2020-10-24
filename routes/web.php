746c41c26d8bc7df509d934851d6afff0a2a4079
EricTalv <eric.talviste@tptlive.ee>
2020-10-24 16:10:02 +0300

    using paginate and rendering data

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,8 @@ Route::get('/about', function () {
     return view('about', [
         'articles' => App\Article::take(3)->latest()->get() ]);
 });
+
+Route::get('/articles', function () {
+    return view('articles', [
+        'articles' => App\Article::latest()->paginate(2) ]);
+});
