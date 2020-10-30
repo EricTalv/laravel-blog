d866d6948bcca8afd9bc8b2436c9ab4e71f0c4ab
EricTalv <eric.talviste@tptlive.ee>
2020-10-30 16:40:41 +0200

    migration works, fixed bug

--- database/migrations/2020_10_30_143415_create_articles_table.php
+++ database/migrations/2020_10_30_143415_create_articles_table.php
@@ -18,8 +18,6 @@ class CreateArticlesTable extends Migration
             $table->string('title');
             $table->string('excerpt');
             $table->text('body');
-            $table->timestamps('created_at');
-            $table->timestamps('updated_at');
 
 
 
