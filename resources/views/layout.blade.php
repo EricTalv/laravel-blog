fb836ae1efdaf5ad73529d19fb67561d64017567
EricTalv <eric.talviste@tptlive.ee>
2020-10-30 16:19:39 +0200

    testing day

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,18 +22,7 @@
         </div>
         <div id="menu">
             <ul>
-                <li class="{{ Request::path() === '/' ? 'current_page_item' : ''}}"><a href="/" accesskey="1" title="">Homepage</a>
-                </li>
-                <li class="{{ Request::path() === 'clients' ? 'current_page_item' : ''}}"><a href="#" accesskey="2"
-                                                                                             title="">Our Clients</a>
-                </li>
-                <li class="{{ Request::is('about') ? 'current_page_item' : '' }}"><a href="/about" accesskey="3"
-                                                                                     title="">About Us</a></li>
-                <li class="{{ Request::path() === 'articles' ? 'current_page_item' : ''}}"><a href="/articles"
-                                                                                              accesskey="4" title="">Articles</a>
-                </li>
-                <li class="{{ Request::path() === 'contact' ? 'current_page_item' : ''}}"><a href="#" accesskey="5"
-                                                                                             title="">Contact Us</a>
+                <li class="current_page_item"><a href="/" accesskey="1" title="">Homepage</a>
                 </li>
             </ul>
         </div>
