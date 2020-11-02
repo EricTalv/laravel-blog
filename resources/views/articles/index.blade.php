c4eb95bc10dc1ca06a636ed12d82761bfe168315
EricTalv <eric.talviste@tptlive.ee>
2020-11-02 20:59:41 +0200

    tinkering with the frontend

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,24 +1,31 @@
 @extends('layout')
 
+@section('head')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.css">
+@endsection
+
 @section('content')
     <div id="wrapper">
         <div id="page" class="container">
-            <div id="content">
-                <div class="title">
-                    <h2>articles</h2>
-                </div>
-                @if (!$articles)
-                     <h1>No articles</h1>
+            <div id="content is-large">
+
+                @if (!$articles->isEmpty())
+                    <ul>
+                        <li>
+                            @foreach ($articles as $article)
+                                <div class="level">
+                                    <h1><a href="/articles/{{ $rticle->id }}">{{ $article->title }}</a></h1>
+                                    <p>{{ $article->excerpt }}</p>
+                                    <p><small>{{ $article->created_at }}</small></p>
+                                    <button><a href="/articles/edit/{{ $article->id }}">Edit</a></button>
+                                </div>
+                            @endforeach
+                        </li>
+                    </ul>
                 @else
-                <ul>
-                    <li>
-                        @foreach ($articles as $article)
-                            <h1><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></h1>
-                            <pre>{{ $article->excerpt }}</pre>
-                            <p><small>{{ $article->created_at }}</small></p>
-                        @endforeach
-                    </li>
-                </ul>
+                    <div class="title">
+                        <h2>No articles available</h2>
+                    </div>
                 @endif
             </div>
         </div>
