48e8586696bdd058aabf53a21bae8643d7a66b08
EricTalv <eric.talviste@tptlive.ee>
2020-11-15 16:03:36 +0200

    finished article create from

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -16,151 +16,65 @@
 @section('content')
 
     <div class="container">
-        <form method="POST" action="{{ route('articles.store') }}">
+        <form method="POST" action="{{ route('articles.store') }}" novalidate>
+            @csrf
+            @method('PUT')
+
             <div class="form-group">
-                <label for="exampleFormControlInput1">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <label for="title"><h4>Title</h4></label>
+                <input
+                    {{-- If we have an error, add is-danger class  --}}
+                    class="form-control {{ !$errors->has('title') ?  '' : 'invalid-feedback' }}"
+                    type="text"
+                    name="title"
+                    id="title"
+                    {{-- Retreives an old input item --}}
+                    value="{{ old('title') }}"
+                >
             </div>
+            @error('title')
+            <p class="invalid-feedback">{{ $message }}</p>
+            @enderror
             <div class="form-group">
-                <label for="exampleFormControlSelect1">Example select</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <label for="excerpt"><h4>Excerpt</h4></label>
+                <textarea
+                    class="form-control {{ !$errors->has('excerpt') ?  '' : 'invalid-feedback' }}"
+                    name="excerpt"
+                    rows="3"
+                    id="excerpt">{{ old('excerpt') }}</textarea>
+            </div>
+            @error('excerpt')
+            <p class="invalid-feedback">{{ $message }}</p>
+            @enderror
+            <div class="form-group">
+                <label for="body"><h4>Body</h4></label>
+                <textarea
+                    class="form-control {{ !$errors->has('body') ?  '' : 'invalid-feedback' }}"
+                    name="body"
+                    rows="3"
+                    id="body">{{ old('body') }}</textarea>
+            </div>
+            @error('body')
+            <p class="invalid-feedback">{{ $message }}</p>
+            @enderror
+            <div class="form-group">
+                <label for="existing-tags-input"><h4>Tags</h4></label>
+                {{--
+                           tags[] enables us to fetch multiple (array)
+                           options to send as a request instead of one.
+
+                           This however lets you add EXISTING tags
+                      --}}
+                <select name="tags[]" class="form-control form-control-lg" multiple >
+                    @foreach($tags as $tag)
+                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                    @endforeach
                 </select>
+                <small class="from-text text-muted">Hold down control to select multiple options</small>
             </div>
-            <div class="form-group">
-                <label for="exampleFormControlSelect2">Example multiple select</label>
-                <select multiple class="form-control" id="exampleFormControlSelect2">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
+            @error('tags')
+            <p class="help is-danger">{{ $message }}</p>
+            @enderror
         </form>
-    </div>
-    <div id="wrapper">
-        <div id="page" class="container">
-            <h1 class="heading has-text-weight-bold is-size-4">New Article</h1>
-
-            <form method="POST" action="">
-                @csrf
-                @method('PUT')
-
-                <div class="field">
-                    <label for="title" class="label">Title</label>
-
-                    <div class="control">
-                        <input
-                            {{-- If we have an error, add is-danger class  --}}
-                            class="input {{ !$errors->has('title') ?  '' : 'is-danger' }}"
-                            type="text"
-                            name="title"
-                            id="title"
-                            {{-- Retreives an old input item --}}
-                            value="{{ old('title') }}"
-                        >
-                    </div>
-                    {{--
-                     @error is a helper in blade -> if we have any errors,
-                      this will render and display the error message as $message
-                    --}}
-                    @error('title')
-                    <p class="help is-danger">{{ $message }}</p>
-                    @enderror
-
-                </div>
-
-                <div class="field">
-                    <label for="excerpt" class="label">Excerpt</label>
-
-                    <div class="control">
-                        <textarea
-                            class="textarea {{ !$errors->has('excerpt') ?  '' : 'is-danger' }}"
-                            name="excerpt"
-                            id="excerpt">{{ old('excerpt') }}</textarea>
-                    </div>
-                    @error('excerpt')
-                    <p class="help is-danger">{{ $message }}</p>
-                    @enderror
-
-                </div>
-
-                <div class="field">
-                    <label for="body" class="label">Body</label>
-
-                    <div class="control">
-                        <textarea
-                            class="textarea {{ !$errors->has('body') ?  '' : 'is-danger' }}"
-                            name="body"
-                            id="body">{{ old('body') }}</textarea>
-                    </div>
-                    @error('body')
-                    <p class="help is-danger">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="field">
-                    <label for="tagger" class="label">Tags</label>
-                    <div class="control">
-                        {{--
-
-                            You should be able to write any word into the
-                            input, press enter, this creates a Word-Label.
-
-                            These will be posted and later added to the post.
-
-                            If theres a new TAG it will be created into the
-                            TAGS table.
-                            Otherwise we will just add it.
-
-                        --}}
-                        <input
-                            class="input"
-                            type="text"
-                            name="tagger"
-                            id="tagger"
-                        >
-                        <script>
-
-                        </script>
-                    </div>
-                </div>
-
-                <div class="field">
-                    <label for="tagger" class="label">Existing tags</label>
-                    <div class="control">
-                        {{--
-                             tags[] enables us to fetch multiple (array)
-                             options to send as a request instead of one.
-
-                             This however lets you add EXISTING tags
-                        --}}
-                        <select name="tags[]" class="select is-multiple" multiple>
-                            @foreach($tags as $tag)
-                                <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    @error('tags')
-                    <p class="help is-danger">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link" type="submit">Submit</button>
-                    </div>
-                </div>
-            </form>
-        </div>
     </div>
 @endsection
