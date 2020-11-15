25e2e2a51f100fa3978af69c8c3f10f157df9e11
EricTalv <eric.talviste@tptlive.ee>
2020-11-15 18:39:06 +0200

    tinkering with dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -28,6 +29,9 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        return view('dashboard',
+            [
+            'latestArticle' => Article::latest()->first(),
+        ]);
     }
 }
