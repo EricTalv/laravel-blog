edf4fc61a8ccfa0d4c2218a928324da814c9bf0d
erictalv <eric.talviste@tptlive.ee>
2020-12-26 17:57:53 +0200

    design nitpicks

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -32,8 +32,8 @@
         </div>
         <div class="row w-75 ">
             <div class="col-12 p-0">
-                <div class="comment-input mb-3">
-                    <div class="comment px-3 py-3 bg-white border border-secondary">
+                <div class="comment-input mb-3 ">
+                    <div class="comment px-3 py-3 bg-white rounded">
                         <img src="https://picsum.photos/80" class="avatar float-left mr-2">
                         <div class="row">
                             <div class="comment-input-data w-100">
@@ -56,8 +56,7 @@
                 </div>
             </div>
             <div class="comments w-100">
-
-                <div class="comment px-3 py-3 bg-white">
+                <div class="comment px-3 py-3 bg-white rounded">
                     <img src="https://picsum.photos/80" class="avatar float-left mr-2">
                     <div class="row">
                         <div class="comment-data">
