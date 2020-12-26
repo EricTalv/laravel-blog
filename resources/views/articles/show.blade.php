fcbdb5d8431fa1bdd1cb3202af49c40244e20d60
erictalv <eric.talviste@tptlive.ee>
2020-12-26 17:53:38 +0200

    adding commenting input

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -32,45 +32,51 @@
         </div>
         <div class="row w-75 ">
             <div class="col-12 p-0">
-                <div class="comment-input"></div>
-                <div class="comments w-100">
-                    <div class="comment pr-5 bg-white mb-3">
-                        <div class="clearfix">
-                            <img src="https://picsum.photos/80" class="avatar float-left mr-2">
-                            <div class="comment-data">
-                                <div class="username font-weight-bold">Freekdude20 <small class="text-muted small">12 Dec, 2020</small>
+                <div class="comment-input mb-3">
+                    <div class="comment px-3 py-3 bg-white border border-secondary">
+                        <img src="https://picsum.photos/80" class="avatar float-left mr-2">
+                        <div class="row">
+                            <div class="comment-input-data w-100">
+                                <div class="username font-weight-bold">William Shakesphear The Third<small
+                                        class="text-muted small ml-2">12 Dec, 2020</small>
                                 </div>
-                                <div class="comment-text">This is really cool!</div>
-                                <div class="comment-buttons float-right my-1 mr-4">
-                                    <button class="btn btn-outline-success">Like</button>
-                                    <button class="btn btn-outline-primary">Reply</button>
+                                <div class="comment-text-input">
+                                    <form>
+                                        <div class="form-group">
+                                            <textarea class="form-control" class="comment-input" rows="5" ></textarea>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="comment-buttons float-right ">
+                                    <button class="btn btn-outline-primary">Submit</button>
                                 </div>
                             </div>
-
                         </div>
-
-
                     </div>
-                    <div class="comment pr-5  bg-white">
-                        <div class="clearfix">
-                            <img src="https://picsum.photos/80" class="avatar float-left mr-2">
-                            <div class="comment-data">
-                                <div class="username font-weight-bold">William Shakesphear The Third<small class="text-muted small">12 Dec, 2020</small>
-                                </div>
-                                <div class="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequuntur deleniti earum ex, fugit iure laborum modi? Accusantium ad aliquid architecto autem consequatur culpa dolore eaque eligendi est et excepturi facere harum iusto laboriosam magni minus nemo non numquam odit officia officiis perferendis perspiciatis placeat quae quod ratione reprehenderit repudiandae, sequi soluta totam ut vel vitae voluptatem. Ad aliquam aliquid animi aspernatur assumenda at atque aut beatae cum doloribus ducimus eaque eveniet fugiat impedit ipsa ipsam modi molestias necessitatibus nesciunt obcaecati odit, officiis provident quibusdam reiciendis sunt veniam voluptas voluptatibus? Delectus deserunt dicta distinctio doloribus ea esse eveniet excepturi expedita facere magnam, maiores mollitia natus nesciunt nostrum nulla odit perspiciatis possimus provident quae quisquam quos repellendus reprehenderit sed sit totam velit voluptate! Dolor explicabo fuga laudantium minima nesciunt nostrum numquam provident quae quia quidem reprehenderit tempora totam veritatis, voluptas voluptatum! Ad at cumque dolor dolores excepturi facere minus possimus rem.</div>
-                                <div class="comment-buttons float-right my-1 mr-4">
-                                    <button class="btn btn-outline-success">Like</button>
-                                    <button class="btn btn-outline-primary">Reply</button>
-                                </div>
+                </div>
+            </div>
+            <div class="comments w-100">
+
+                <div class="comment px-3 py-3 bg-white">
+                    <img src="https://picsum.photos/80" class="avatar float-left mr-2">
+                    <div class="row">
+                        <div class="comment-data">
+                            <div class="username font-weight-bold">William Shakesphear The Third<small
+                                    class="text-muted small ml-2">12 Dec, 2020</small>
                             </div>
-
+                            <div class="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Consectetur consequuntur deleniti earum ex
+                            </div>
+                            <div class="comment-buttons float-right my-1 mr-4">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-primary">Reply</button>
+                            </div>
                         </div>
-
-
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 
