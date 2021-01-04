1babbaec3e5db934ee4d4bd0f04a4409360aa8f7
erictalv <eric.talviste@tptlive.ee>
2021-01-04 16:49:51 +0200

    added comments model methods

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+
+    use SoftDeletes;
+
     protected $fillable = ['body'];
+
+    /**
+     * The belongs to Relationship
+     *
+     * @var array
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The has Many Relationship
+     *
+     * @var array
+     */
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
