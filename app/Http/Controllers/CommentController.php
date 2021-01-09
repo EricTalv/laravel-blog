6a6cb97776be921f7a9c781d0d22d32aa24420c6
EricTalv <eric.talviste@tptlive.ee>
2021-01-09 22:40:09 +0200

    creating store method in comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
 
 class CommentController extends Controller
 {
@@ -25,6 +26,9 @@ class CommentController extends Controller
         // Set User Id as current authenticated user
         $input['user_id'] = auth()->user()->id;
 
+        // Create comment
+        Comment::create($input);
 
+        return back();
     }
 }
