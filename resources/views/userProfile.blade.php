2abcfeb3518c034b20bc0c37cb66418067bfe06e
erictalv <eric.talviste@tptlive.ee>
2021-01-11 17:12:04 +0200

    userprofile initial has been implemented

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -4,5 +4,15 @@
 @section('content')
 
     <h1>I am {{ $user->name }}</h1>
+    <ol>
+    @foreach( $user->articles as $article)
+
+           <li>
+               {{ $article->title }}
+           </li>
+
+    @endforeach
+    </ol>
+
 
 @endsection
