9dcd739afa8c4f42be53adff79b9beb374878d7b
erictalv <eric.talviste@tptlive.ee>
2021-01-19 17:49:26 +0200

    changing all links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
             <div class="col-md-6 px-0">
                 <h1 class="display-4 font-italic text-capitalize">{{ $newestArticle->title }}</h1>
                 <p class="lead my-3 ">{{ $newestArticle->excerpt }}</p>
-                <p class="lead mb-0"><a href="/article/{{ $newestArticle->id }}" class="text-white font-weight-bold">Continue
+                <p class="lead mb-0"><a href="{{ $newestArticle->path }}" class="text-white font-weight-bold">Continue
                         reading...</a></p>
             </div>
         </div>
