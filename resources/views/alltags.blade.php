5961ec85389d7dfbf8d516364fc071d2f5df973f
erictalv <eric.talviste@tptlive.ee>
2021-01-22 12:11:34 +0200

    trying to get dynamic styles on alltags page

--- resources/views/alltags.blade.php
+++ resources/views/alltags.blade.php
@@ -6,7 +6,12 @@
         <div class="row">
             @foreach($tags as $tag)
                 <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"
-                   style="height: {{ 35 * $tag->articles()->count() }}% "
+                   style="
+                       height: {{ 35 * $tag->articles()->count() }}px;
+                       width: {{ 35 * $tag->articles()->count() }}px;
+                       font-size: {{ 35 * $tag->articles()->count() }}px;
+
+                       "
                    class="m-3 btn btn-secondary align-items-center">
                     {{ $tag->name }}
                     <span class="badge badge-light">{{ $tag->articles()->count() }}</span>
