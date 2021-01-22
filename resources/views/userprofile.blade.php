14b49c2863cb2d53593c414dc2224a744156a8de
erictalv <eric.talviste@tptlive.ee>
2021-01-22 15:21:11 +0200

    edits to userprofile

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -19,7 +19,7 @@
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">User Profile</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                                class="fas fa-download fa-sm text-white-50"></i> Something fun</a>
                     </div>
 
                     <div class="row">
@@ -128,7 +128,7 @@
                         </div>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="col-xl-6 col-md-12 mb-4">
                             <div class="card border-left-info shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -263,7 +263,7 @@
                                 <div class="card-body">
                                     <div class="text-center">
                                     </div>
-                                    <p>Add a bio</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti est possimus quas ullam. Eligendi, vitae.</p>
 
                                 </div>
                             </div>
@@ -271,14 +271,10 @@
                             <!-- Approach -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Testing</h6>
                                 </div>
                                 <div class="card-body">
-                                    <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
-                                        CSS bloat and poor page performance. Custom CSS classes are used to create
-                                        custom components and custom utility classes.</p>
-                                    <p class="mb-0">Before working with this theme, you should become familiar with the
-                                        Bootstrap framework, especially the utility classes.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolor dolore doloribus et laborum minima nam necessitatibus obcaecati quod repudiandae.</p>
                                 </div>
                             </div>
 
