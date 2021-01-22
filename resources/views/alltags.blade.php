dc6c18c197d8ece4f1e3ab1d0d1b31810b55aca1
erictalv <eric.talviste@tptlive.ee>
2021-01-22 15:15:22 +0200

    configuring userprofile page

--- resources/views/alltags.blade.php
+++ resources/views/alltags.blade.php
@@ -8,12 +8,12 @@
                 <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"
                    style="
                       height: calc(100% + {{ $tag->articles()->count() }}px);
-                      height: 35 *  {{ $tag->articles()->count() }}px;
+
 "
 
                    class="m-3 btn btn-secondary align-items-center">
                     {{ $tag->name }}
-                    <span class="badge badge-light">{{ $tag->articles()->count() }}</span>
+                    <span class="badge badge-light">{{ $tag->articles->count() }}</span>
                 </a>
             @endforeach
         </div>
--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -82,17 +82,7 @@
                                     <div class="chart-pie pt-4 pb-2">
                                         <canvas id="myPieChart"></canvas>
                                     </div>
-                                    <div class="mt-4 text-center small">
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-primary"></i> Direct
-                                        </span>
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-success"></i> Social
-                                        </span>
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-info"></i> Referral
-                                        </span>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -126,8 +116,8 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Last login</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">25 jan, 2020</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -143,11 +133,11 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Comments made
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">523</div>
                                                 </div>
                                                 <div class="col">
                                                     <div class="progress progress-sm mr-2">
@@ -166,23 +156,6 @@
                             </div>
                         </div>
 
-                        <!-- Pending Requests Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
 
                     <!-- Content Row -->
