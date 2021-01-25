b8c2846007bf3e4592d947b4c3427275c71a7455
erictalv <eric.talviste@tptlive.ee>
2021-01-25 14:24:44 +0200

    fixing naming scheme errors

--- app/Http/Middleware/LastOnlineAt.php
+++ app/Http/Middleware/LastOnlineAt.php
@@ -28,11 +28,11 @@ class LastOnlineAt
          *  If its more than an hour, update DB time to current time
          */
 
-        if (auth()->user()->last_online_at->diffInHours(now()) !== 0)
+        if (auth()->user()->last_login_at->diffInHours(now()) !== 0)
         {
             DB::table("users")
                 ->where("id", auth()->user()->id)
-                ->update(["last_online_at" => now()]);
+                ->update(["last_login_at" => now()]);
         }
 
         return $next($request);
