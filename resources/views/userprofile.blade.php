0637d69029128bb688fbf6a15761fa0bb118d39e
erictalv <eric.talviste@tptlive.ee>
2021-01-26 20:47:04 +0200

    editing userprofile

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -49,7 +49,7 @@
                                 <!-- Card Body -->
                                 <div class="card-body">
                                     <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
+                                      card Body
                                     </div>
                                 </div>
                             </div>
