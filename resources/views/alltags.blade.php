fb70367e8f1830e7ce26d3ecec5059853caaa1a7
EricTalv <eric.talviste@tptlive.ee>
2021-02-01 18:10:58 +0200

    fixed tag bug

--- resources/views/alltags.blade.php
+++ resources/views/alltags.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>All Tags</h1>
-        <div class="tagCloud" style="align-items: flex-start;">
-            @foreach($tags as $tag)
-                <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"
-                   style="
-                       font-size:  {{ ( 15 * $tag->articles()->count()) }}px;
+<div class="container">
+    <h1>All Tags</h1>
+    <div class="tagCloud" style="align-items: flex-start;">
+        @foreach($tags as $tag)
 
-                       align-items: center;
-                       white-space: nowrap;
-                       "
-                   class="m-3 btn btn-secondary  ">
+            @if($tag->articles()->count() !== 0)
+
+                <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"
+                    style="
+                   font-size:  {{ ( 15 * $tag->articles()->count()) }}px;
+
+                   align-items: center;
+                   white-space: nowrap;
+                   " class="m-3 btn btn-secondary  ">
                     {{ $tag->name }}
-                    <span class="badge badge-light mx-1">{{ $tag->articles()->count() }}</span>
+                    <s pan class="badge badge-light mx-1">{{ $tag->articles()->count() }}</span>
                 </a>
-            @endforeach
-        </div>
+
+            @endif
+        @endforeach
     </div>
+</div>
 @endsection
