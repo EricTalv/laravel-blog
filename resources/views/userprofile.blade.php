70be1be4dffe1aa947b8cec70e2d6d5bf8ea3b73
EricTalv <eric.talviste@tptlive.ee>
2021-02-04 17:28:17 +0200

    cleaning up code

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -92,7 +92,7 @@
                     <div class="row">
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="col-xl-4 col-md-4 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -110,7 +110,7 @@
                         </div>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="col-xl-4 col-md-4 mb-4">
                             <div class="card border-left-success shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -128,7 +128,7 @@
                         </div>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-6 col-md-12 mb-4">
+                        <div class="col-xl-4 col-md-4 mb-4">
                             <div class="card border-left-info shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -138,13 +138,6 @@
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
                                                     <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">523</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                             style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                             aria-valuemax="100"></div>
-                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -161,12 +154,11 @@
                     <!-- Content Row -->
 
 
-                    <!-- Content Row -->
+                    <!-- Bio & Latest Article -->
                     <div class="row">
 
+                        {{-- BIO START --}}
                         <div class="col-lg-6 mb-4">
-
-                            <!-- Illustrations -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Bio</h6>
@@ -179,6 +171,25 @@
                                 </div>
                             </div>
                         </div>
+                        {{-- BIO END --}}
+
+
+                        {{-- LATEST ARTICLE START --}}
+                        <div class="col-lg-6 mb-4">
+
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Latest Article</h6>
+                                </div>
+                                <div class="card-body">
+                                    <h1>Title</h1>
+                                    <p>Excerpt Lorem ipsum dolor sit amet.</p>      
+                                    <button class="btn btn-primary">Go</button>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- LATEST ARTICLE END --}}
+
                     </div>
 
                 </div>
